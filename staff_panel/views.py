097307aa8dbf909a3814a617a629dcee8d6c3d32
Aditya Pushkar <adityapushkar850@gmail.com>
2022-04-05 19:24:24 +0530

    nft verification with crypto reward

--- staff_panel/views.py
+++ staff_panel/views.py
@@ -1,12 +1,13 @@
 """       nft approval and rejection panel for staff      """
 
-from functools import partial
+from django.db.models import F
 from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.response import Response
 from django.db.models import Q
 from rest_framework.pagination import PageNumberPagination
 
+from ethereum.models import EthBalance
 from nft.models import Nft
 from nft.serializers import NftSerializer
 
@@ -33,6 +34,29 @@ def nft_approve(request, pk):
         serializer = NftSerializer(nft, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
+
+            # update the eth balance of an user
+            """
+                User get rewarded on every NFT they uplode.
+                User only get eth after we approved/verified the NFT uploded by the user
+            """
+            if nft.approved == True:
+                try:
+                    eth_user = EthBalance.objects.get(user=nft.user)
+                    if eth_user:
+                        eth_user.eth_balance = F('eth_balance')+0.00000017
+                        eth_user.save()
+                        print("SUCCESSFULL UPDATED")
+                
+                #  If user uplode's their first NFT
+                except Exception as e:
+                    print("CREATING ETH WALLET")
+                    eth_user = EthBalance.objects.create(
+                            user = nft.user
+                        )
+                    eth_user.save()
+                    print("SUCCESS")       
+
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(status=status.HTTP_204_NO_CONTENT)
